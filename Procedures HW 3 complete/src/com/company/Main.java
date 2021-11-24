6274dcb1b284f5bb521b4d3ad2a1aa374d204d42
Lukaamechi <89980398+Lukaamechi@users.noreply.github.com>
2021-11-24 10:22:07 +0200

    Update Main.java

--- Procedures HW 3 complete/src/com/company/Main.java
+++ Procedures HW 3 complete/src/com/company/Main.java
@@ -2,21 +2,71 @@ package com.company;
 
 public class Main {
 
+//hw 2
     public static void main(String[] args) {
-        int[] array1 = {32, 4, 242, 4, 24, 2, 42, 4, 24, 2, 42, 42, 4};
-        int[] array2 = {6, 63, 5, 35, 35, 3, 87, 7, 87};
-        System.out.println(any(array1, array2));
+        Book[] books = new Book[2];
+        books[0] = new Book("Metamorphosis", new String[]{"Franz Kafka"}, 1915);
+        books[1] = new Book("Demian", new String[]{"Hermann Hesse"}, 1919);
+        for (Book book : books)
+            System.out.println(book);
     }
-    static boolean contains (int[] array, int value) {
-        for (int i = 0; i< array.length; i++)
-            if (array[i] == value) return true;
-        return false;
-    }
-
-    static boolean any(int[] a, int[] b) {
-        for (int i = 0; i < a.length; i++)
-            if (contains(b, a[i])) return true;
-        return false;
-    }
-
 }
+
+class Book
+{
+    public Book (String title, String[] authors, int publicationYear)
+    {
+        this.title = title;
+        this.authors = authors;
+        this.publicationYear = publicationYear;
+    }
+
+    public String toString ()
+    {
+        StringBuffer strBuffer = new StringBuffer ();
+        int numAuth = authors.length;
+        for (int i = 0; i < numAuth; i = i + 1)
+        {
+            strBuffer.append (authors [i]);
+            if (i != numAuth - 1)
+                strBuffer.append (" & ");
+        }
+        strBuffer.append (": " + title + " ");
+        strBuffer.append ("(" + publicationYear + ")");
+
+        return strBuffer.toString ();
+    }
+
+    public String getTitle ()
+    {
+        return title;
+    }
+
+    public String[] getAuthors ()
+    {
+        return authors;
+    }
+
+    public int getPublicationYear ()
+    {
+        return publicationYear;
+    }
+
+    public int numAuthors ()
+    {
+        return authors.length;
+    }
+
+    public boolean newerThan (Book book)
+    {
+        return publicationYear > book.getPublicationYear ();
+    }
+
+    private String title;
+    private String[] authors;
+    private int publicationYear;
+}
+
+
+
+
